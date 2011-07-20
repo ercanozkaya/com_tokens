37c38a22e239cca87d105d5b7af0489d89702562
Ercan Özkaya <ozkayaercan@gmail.com>
2011-07-21 00:56:44 +0300

    Fix exception catching condition
    Only intercept the exception if format is json

--- code/components/com_tokens/tokens.php
+++ code/components/com_tokens/tokens.php
@@ -3,10 +3,15 @@ try {
 	echo KFactory::get('site::com.tokens.dispatcher')->dispatch();	
 }
 catch (KException $e) {
-	$response = new stdclass;
-	$response->error = true;
-	$response->code = KHttpResponse::isError($e->getCode()) ? $e->getCode() : 500;
-	$response->message = $e->getMessage();
-	
-	echo json_encode($response);
+	if (KRequest::format() == 'json') {
+		$response = new stdclass;
+		$response->error = true;
+		$response->code = KHttpResponse::isError($e->getCode()) ? $e->getCode() : 500;
+		$response->message = $e->getMessage();
+		
+		echo json_encode($response);
+	}
+	else {
+		throw $e;
+	}
 }
